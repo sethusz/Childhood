5b05df598f8b0befa0a906ca735e17c02849ad04
Oleksandr <oleksandrzelenjak@gmail.com>
2023-12-12 00:47:45 +0200

    updated func

--- wp-content/themes/childhood/index.php
+++ wp-content/themes/childhood/index.php
@@ -1,11 +1,74 @@
 <?php
-  get_header();
+get_header();
 ?>
 
         <div class="mainslider glide">
             <div class="glide__track" data-glide-el="track">
                 <ul class="glide__slides">
-                    <li style="background-image: url('<?php echo bloginfo('template_url'); ?>/assets/img/bg_1.jpg')" class="glide__slide">
+                <?php
+
+                $posts = get_posts(array(
+                    'numberposts' => -1,
+                    'category_name' => 'slider',
+                    'orderby' => 'date',
+                    'order' => 'ASC',
+                    'post_type' => 'post',
+                    'suppress_filters' => true,
+                ));
+
+                foreach ($posts as $post) {
+                    setup_postdata($post);
+                    ?>
+                            <li style="background-image: url('<?php the_field('slider_img'); ?>')" class="glide__slide">
+                                <div class="container">
+                                    <div class="row">
+                                        <div class="col-lg-7 offset-1">
+                                            <h2
+                                            style="
+                                        <?php
+                                        $field = get_field('slider_color');
+
+                                        if ($field == 'white') {
+                                            ?>
+                                                color: #fff;
+                                            <?php
+                                        }
+                                        ?>
+                                        "
+                                             class="slider__title"><?php the_title('') ?></h2>
+
+
+                                             <?php
+                                             $field = get_field('slider_btn');
+
+                                             if ($field == 'on') {
+                                                 ?>
+                                                <a href="<?php the_field('slider_link') ?>" class="button">Узнать больше</a>
+                                                    <?php
+                                             }
+                                             ?>
+                                        </div>
+                                    </div>
+                                    <button class="glide__arrow glide__arrow--left" data-glide-dir="<">
+                                        <svg width="15" height="25" viewBox="0 0 15 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M0.982942 13.3923L12.2253 24.631C12.7186 25.123 13.5179 25.123 14.0124 24.631C14.5057 24.1389 14.5057 23.3397 14.0124 22.8476L3.66178 12.5007L14.0112 2.15378C14.5045 1.66172 14.5045 0.862477 14.0112 0.369169C13.5179 -0.122894 12.7174 -0.122894 12.2241 0.369169L0.981696 11.6077C0.495966 12.0947 0.495966 12.9065 0.982942 13.3923Z" fill="white"/>
+                                        </svg>
+                                    </button>
+                                    <button class="glide__arrow glide__arrow--right" data-glide-dir=">">
+                                        <svg width="15" height="25" viewBox="0 0 15 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M14.0171 11.6077L2.77467 0.369029C2.28137 -0.123032 1.48213 -0.123032 0.987571 0.369029C0.494263 0.861093 0.494264 1.66033 0.987572 2.15239L11.3382 12.4993L0.98882 22.8462C0.495512 23.3383 0.495512 24.1375 0.98882 24.6308C1.48213 25.1229 2.28261 25.1229 2.77592 24.6308L14.0183 13.3923C14.504 12.9053 14.504 12.0935 14.0171 11.6077Z" fill="white"/>
+                                        </svg>
+                                    </button>
+                                </div>
+                            </li>
+                                            <?php
+                }
+
+                wp_reset_postdata();
+
+                ?>
+
+                    <!-- <li style="background-image: url('<?php echo bloginfo('template_url'); ?>/assets/img/bg_1.jpg')" class="glide__slide">
                         <div class="container">
                             <div class="row">
                                 <div class="col-lg-7 offset-1">
@@ -64,7 +127,7 @@
                                 </button>
                             </div>
                         </div>
-                    </li>
+                    </li> -->
                 </ul>
             </div>
         </div>
@@ -73,16 +136,23 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-10 offset-md-1 col-lg-5 offset-lg-1">
-                        <div class="about__img">
-                            <img src="<?php echo bloginfo('template_url'); ?>/assets/img/about.jpg" alt="про компанию">
-                        </div>
+                    <div class="about_img">
+    <?php
+    $image = get_field('about_img');
+
+    if (!empty($image)): ?>
+                <img src="<?php echo $image['url']; ?>" 
+                     alt="<?php echo $image['alt']; ?>"
+                     style="max-width: 100%; height: auto;">
+    <?php endif;
+    ?>
+</div>
+
                     </div>
                     <div class="col-md-10 offset-md-1 offset-lg-0 col-lg-6 col-xl-5 offset-xl-1">
-                        <h1 class="title underlined">Про компанию</h1>
+                        <h1 class="title underlined"><?php the_field('about_title'); ?></php></h1>
                         <div class="about__text">
-                            Наша компания уже больше десяти лет дарит позитивные эмоции детям и их родителям. Мы воплощаем все детские мечты и помогаем родителям дарить счастливое детство!
-                            <br> <br>
-                            Но и взрослые иногда так нуждаются в детских эмоциях! Мы можем и это, ведь так приятно почувствовать заботу даже когда тебе за... :)
+                        <?php the_field('about_description'); ?>
                         </div>
                         <a href="#" class="button">Узнать больше</a>
                     </div>
@@ -91,10 +161,19 @@
         </div>
         <div class="specialists" id="specialists">
             <div class="container">
-                <div class="title">Наша команда</div>
+                <div class="title"><?php the_field('team_title'); ?></div>
                 <div class="row">
                     <div class="col-lg-10 offset-lg-1">
-                        <img class="specialists__img" src="<?php echo bloginfo('template_url'); ?>/assets/img/team.jpg" alt="наша команда">
+                    <div class="about_img">
+    <?php
+    $image = get_field('team_img');
+
+    if (!empty($image)): ?>
+                <img src="<?php echo $image['url']; ?>" 
+                     alt="<?php echo $image['alt']; ?>"
+                     class="specialists__img">
+    <?php endif;
+    ?>
                     </div>
                 </div>
             </div>
@@ -199,35 +278,33 @@
 
         <div class="aboutus" id="aboutus">
             <div class="container">
-                <h1 class="title">Наша история</h1>
+                <h1 class="title"><?php the_field('history_title') ?></h1>
                 <div class="row">
                     <div class="col-lg-6">
                         <div class="subtitle">
-                            Все начиналось с желания
+                        <?php the_field('history_subtitle_1') ?>
                         </div>
                         <div class="aboutus__text">
-                            Желания сделать как можно больше детей счастливыми. Именно с этой идеи все и зарождалось.
-                            <br><br>
-                            Первые игрушки, сделанные вручную были классическими плюшевыми медведями, которые разошлись настолько быстро, что нас завалили заказами на несколько месяцев вперед. Именно в то время мы поняли, что идем правильным путем, вкладывая все силы и эмоции в наши игрушки.
+                        <?php the_field('history_descr_1') ?>
                         </div>
                     </div>
                     <div class="col-lg-6">
-                        <img class="aboutus__img" src="<?php echo bloginfo('template_url'); ?>/assets/img/about_1.jpg" alt="мир детства">
+                    <img class="aboutus__img" src="<?php the_field('history_img_1'); ?>" alt="мир детства">
+
                     </div>
                 </div>
 
                 <div class="row">
                     <div class="col-lg-6">
-                        <img class="aboutus__img" src="<?php echo bloginfo('template_url'); ?>/assets/img/about_2.jpg" alt="мир детства">
-                    </div>
+                    <img class="aboutus__img" src="<?php the_field('history_img_2'); ?>" alt="мир детства">
+         </div>
                     <div class="col-lg-6">
                         <div class="subtitle">
-                            Главное - качество
+                        <?php the_field('history_subtitle_2') ?>
                         </div>
                         <div class="aboutus__text">
-                            Мы делали их вручную, из лучших материалов и не жалея времени. Но мы росли и наш ассортимент расширился и фабричными изделиями.
-                            <br><br>
-                            Выбирая нас, вы можете быть уверены, что мы всегда следим за качеством закупок и никогда не предоставим вам опасный или некачественный товар.
+                        <?php the_field('history_descr_2') ?>
+
                         </div>
                     </div>
                 </div>
@@ -235,17 +312,16 @@
                 <div class="row">
                     <div class="col-lg-6">
                         <div class="subtitle">
-                            Современные технологии
+                        <?php the_field('history_subtitle_3') ?>
                         </div>
                         <div class="aboutus__text">
-                            И даже спустя столько лет мы продолжаем создавать игрушки вручную. Как самые простые, так и те, что идут в ногу со временем. Добавляя электроники и оживляя лучших друзей ребятишек, мы всегда следим за качеством и безопасностью. Каждая отдельная игрушка проходит индивидуальный контроль по всем необходимым стандартам.
-                            <br><br>
-                            Ведь счастливое лицо ребенка - это лучшая награда для нас!
+                        <?php the_field('history_descr_3') ?>
+
                         </div>
                     </div>
                     <div class="col-lg-6">
-                        <img class="aboutus__img" src="<?php echo bloginfo('template_url'); ?>/assets/img/about_3.jpg" alt="мир детства">
-                    </div>
+                    <img class="aboutus__img" src="<?php the_field('history_img_3'); ?>" alt="мир детства">
+     </div>
                 </div>
             </div>
         </div>
@@ -256,9 +332,7 @@
                 <div class="row">
                     <div class="col-lg-6">
                         <div class="contacts__descr underlined">
-                            Мы находимся в Москве, метро "Парк победы", в деловом центре "Парк победы", второй этаж
-                            <br> <br>
-                            по адресу ул. Василисы Кожиной, 1
+                           <?php the_field('our_adress') ?>
                         </div>
                     </div>
                     <div class="col-lg-6">
@@ -318,7 +392,7 @@
                                             </linearGradient>
                                             </defs>
                                         </svg>
-                                        <a href="tel:+797867834347">+797867834347</a>
+                                        <a href="tel:<?php the_field('number_phone_1') ?>"><?php the_field('number_phone_1') ?></a>
                                     </div>
                                 </div>
                                 <div class="contacts__phoneblock">
@@ -368,13 +442,13 @@
                                             </linearGradient>
                                             </defs>
                                         </svg>
-                                        <a href="tel:+797867834358">+797867834358</a>
+                                        <a href="tel:<?php the_field('number_phone_2') ?>"><?php the_field('number_phone_2') ?></a>
                                     </div>
                                 </div>
                             </div>
                             <div class="contacts__mail">
                                 Или напишите нам на почту
-                                <a href="mailto:mirdetstva@gmail.com">mirdetstva@gmail.com</a>
+                                <a href="mailto:<?php the_field('mail'); ?>" class="header__contacts-mail"><?php the_field('mail'); ?></a>
                             </div>
                         </div>
                     </div>
@@ -469,6 +543,7 @@
                         <div class="feedslider glide">
                             <div class="glide__track" data-glide-el="track">
                                 <ul class="glide__slides">
+
                                     <li class="glide__slide">
                                         <div class="feedslider__title">
                                             Иванов Игорь
@@ -505,26 +580,3 @@
         </div>
 
       <?php get_footer(); ?>
-
-      <?php
-    function print_hello() {
-      echo 'Hello world';
-    };
-
-    function print_hello_1() {
-      echo 'Hello world1';
-    };
-
-    function print_hello_2() {
-      echo 'Hello world1';
-    };
-
-
-    add_action('my_hook', 'print_hello'); 
-    add_action('my_hook', 'print_hello_1'); 
-    add_action('my_hook', 'print_hello_2'); 
-
-
-    do_action( 'my_hook' )
-
-?>
